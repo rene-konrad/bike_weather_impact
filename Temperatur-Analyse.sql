8013cb0f17c9d6d0e1724ed53fc28c2fa065cae0
rene-konrad <renekonrad03@gmail.com>
2025-12-23 11:31:55 +0100

    Update Temperatur-Analyse.sql

--- Temperatur-Analyse.sql
+++ Temperatur-Analyse.sql
@@ -1,15 +1,16 @@
 WITH daily_counts AS (SELECT
     COUNT(trips.trip_id) AS count_trips,
-    EXTRACT(MONTH FROM trips.start_date) as monat,
+    EXTRACT(MONTH FROM trips.start_date) AS monat,
     DATE(trips.start_date) AS datum,
+    AVG((weather_2017.temp - 32) / 1.8) AS temperature_celsius,
 CASE
-    WHEN weather_2017.temp < 50 THEN '1_Cold (<50°F)'
-    WHEN weather_2017.temp >= 50 AND weather_2017.temp < 60 THEN '2_Cool_Foggy (50-60°F)'
-    WHEN weather_2017.temp >= 60 AND weather_2017.temp < 68 THEN '3_Ideal (60-68°F)'
-    WHEN weather_2017.temp >= 68 AND weather_2017.temp <= 75 THEN '4_Warm (68-75°F)'
-    WHEN weather_2017.temp > 75 THEN '5_Hot (>75°F)'
-    ELSE 'Unknown'
-END AS comfort_category,
+        WHEN weather_2017.temp < 50 THEN '< 10°C'
+        WHEN weather_2017.temp >= 50 AND weather_2017.temp < 60 THEN '10 - 16°C'
+        WHEN weather_2017.temp >= 60 AND weather_2017.temp < 68 THEN '16 - 20°C'
+        WHEN weather_2017.temp >= 68 AND weather_2017.temp <= 75 THEN '20 - 24°C'
+        WHEN weather_2017.temp > 75 THEN '> 24°C'
+        ELSE 'Unknown'
+    END AS comfort_category,
 CASE 
             WHEN EXTRACT(DAYOFWEEK FROM trips.start_date) IN (1, 7) THEN 'Weekend' 
             ELSE 'Weekday' 
@@ -30,11 +31,12 @@ ON
     trips.start_station_name = station.name
 WHERE
     weather_2017.stn = "724940"
-GROUP BY monat,day_type, comfort_category, rain_status, DATE(trips.start_date))
+GROUP BY monat,day_type, comfort_category,rain_status, DATE(trips.start_date))
 SELECT
     monat,
     rain_status,comfort_category,
-    ROUND(AVG(count_trips),0) AS anzahl_trips_pro_tag
+    ROUND(AVG(count_trips),0) AS anzahl_trips_pro_tag,
+    AVG(temperature_celsius) AS celsius
 FROM
     daily_counts
 WHERE day_type = 'Weekday'
